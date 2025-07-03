e59b7ac405d35abbc34cacba801e9b5bcb536867
varshithab-bms <varshithab.cs23@bmsce.ac.in>
2025-07-03 19:36:07 +0530

    Update profile.ts

--- src/utils/profile.ts
+++ src/utils/profile.ts
@@ -1,6 +1,6 @@
 import { Profile } from "../types";
 
-const API_BASE = "http://192.168.43.111:5000/api/profiles/me"; // current user's profile endpoint
+const API_BASE = "https://expenza-backend-9l3h.onrender.com/api/profiles/me"; 
 const TOKEN_KEY = "auth_token";
 
 // Helper to get JWT token
